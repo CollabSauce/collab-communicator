99246c727f7b3992480a6231bed6332db84d42d5
Brandon Cooper <cooperjbrandon@gmail.com>
2020-09-21 22:44:24 -0700

    put assets in public folder. hardcode to netlify

--- src/scripts/createBanner.js
+++ src/scripts/createBanner.js
@@ -28,7 +28,7 @@ export const createBanner = (onExit) => {
 
   // Create the elements that will live inside the shadowDom
   const downArrow = document.createElement('img');
-  downArrow.src = '../assets/down-arrow.png';
+  downArrow.src = 'https://staging-collab-widget.netlify.app/public/assets/down-arrow.png';
   downArrow.height = 15;
   downArrow.style.cursor = 'pointer';
   downArrow.onclick = () => {
@@ -39,7 +39,7 @@ export const createBanner = (onExit) => {
   };
 
   const upArrow = document.createElement('img');
-  upArrow.src = '../assets/up-arrow.png';
+  upArrow.src = 'https://staging-collab-widget.netlify.app/public/assets/up-arrow.png';
   upArrow.height = 15;
   upArrow.style.cursor = 'pointer';
   upArrow.style.display = 'none';
@@ -54,7 +54,7 @@ export const createBanner = (onExit) => {
   text.innerHTML = 'Click anywhere to leave a comment or edit design';
 
   const cancel = document.createElement('img');
-  cancel.src = '../assets/cancel.svg';
+  cancel.src = 'https://staging-collab-widget.netlify.app/public/assets/cancel.svg';
   cancel.height = 12;
   cancel.style.cursor = 'pointer';
   cancel.onclick = onExit;
--- src/scripts/widget.js
+++ src/scripts/widget.js
@@ -6,7 +6,7 @@ ready.docReady(() => {
     const s2 = document.createElement("script");
     s2.type = "text/javascript";
     s2.async = true;
-    s2.src = 'https://staging-collab-widget.netlify.app/js/app.79189c5f.js';
+    s2.src = 'https://staging-collab-widget.netlify.app/js/app.1cd69b83.js';
     document.body.appendChild(s2);
   };
 
