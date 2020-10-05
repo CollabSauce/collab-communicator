486ee7f6c489dff95179443e467f59b47725aeb1
Brandon Cooper <cooperjbrandon@gmail.com>
2020-10-05 11:32:20 -0700

    add small comment

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -7,6 +7,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  // devtool: 'hidden-source-map',
   stats: 'errors-only',
   bail: true,
   entry: {
